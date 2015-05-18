2aaa7de16ac2177067d6155c0efa017e9cb6f014
Taiki Iida <track_alien@yahoo.co.jp>
2015-05-18 18:05:26 +0900

    本数をふやしました

--- renshu.py
+++ renshu.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 fig, ax = plt.subplots()
 x = np.linspace(0, 10, 200)
-y = np.sin(x)
-ax.plot(x, y, 'b-', linewidth=2)
+for t in range(3):
+    y = np.sin(x) + t
+    ax.plot(x, y, 'b-', linewidth=2)
 plt.show()
